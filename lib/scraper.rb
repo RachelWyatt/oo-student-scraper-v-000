c04350e447e7ed68fb05cd933da520828a0b918a
Rachel Wyatt <rw126@evansville.edu>
2018-10-26 22:48:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,22 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-
+    scraped_students = []
+    #doc.css("div.roster-cards-container").each do |card|
+    #  card.css("div.student-card a").each do |student|
+    #    student_name = student.css(".student-name").first.text,
+    #    student_location = student.css(".student-location").text,
+    #    student_profile_url = student.attr('href')
+    #  scraped_student = {
+    #    name: student_name[0],
+    #    location: student_location,
+    #    profile_url: student_profile_url
+      #}
+      binding.pry
+      scraped_students << scraped_student
+    end
+    end
+    @scraped_students
   end
 
 end
