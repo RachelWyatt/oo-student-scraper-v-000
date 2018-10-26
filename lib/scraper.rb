02e87e53923978eb3c4a81bfa1d704ac0f8deeeb
Rachel Wyatt <rw126@evansville.edu>
2018-10-26 22:38:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
           location: student_location,
           profile_url: student_profile_url
         }
-        #binding.pry
+        binding.pry
         scraped_students << scraped_student
       end
       end
