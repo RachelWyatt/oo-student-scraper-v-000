14fb22759bd2d5663a74ccc62ec8aecef212f169
Rachel Wyatt <rw126@evansville.edu>
2018-10-27 00:07:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,12 +27,12 @@ class Scraper
     profile_page = Nokogiri::HTML(open(profile_url))
     scraped_students = []
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value} #map over the array of 'a' tag objects and return just the attribute 'href' value
-        binding.pry
+
       links.each do |link|
         if link.include?("linkedin")
-          linkedin: link 
+          linkedin: link
         elsif link.include?("twitter")
-          twitter: link 
+          twitter: link
         end
       #now use conditionals to check if the link has a certain string (like 'linkedin') and if it does set key value pairs in student hash
     end
